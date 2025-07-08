96fe6f111ca9a8a9c43e1553cf0492ec23e9ed1b
Михаил Попков <popkov.m@twinscorp.ru>
2025-07-08 15:51:50 +0300

    Подключение к бд + создание моделей и связь их между собой

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import sequelize from "./db.js";
+import cors from "cors";
+import models from "./models/models.js";
+
+const PORT = process.env.PORT || 7000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`);});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
---
+++ backend/models/models.js
@@ -0,0 +1,37 @@
+import sequelize from "../db.js";
+import { DataTypes } from "sequelize";
+
+const User = sequelize.define("user", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING },
+    email: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING },
+});
+
+const Tasks = sequelize.define("tasks", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: { type: DataTypes.STRING },
+    color: { type: DataTypes.STRING },
+    completed: { type: DataTypes.BOOLEAN, defaultValue: false },
+});
+
+const TasksList = sequelize.define("tasksList", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: { type: DataTypes.STRING },
+});
+
+const Readiness = sequelize.define("readiness", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    readiness: { type: DataTypes.BOOLEAN, defaultValue: false },
+});
+
+User.hasMany(Tasks);
+Tasks.belongsTo(User);
+
+Tasks.hasMany(TasksList);
+TasksList.belongsTo(Tasks);
+
+Tasks.hasOne(Readiness);
+Readiness.belongsTo(Tasks);
+
+export default { User, Tasks, TasksList, Readiness };
